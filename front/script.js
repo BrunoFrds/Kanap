f8acce6deacef0c16b2dd9b431eee0a778508b69
Bruno Fernandes <bruno_fernandes@live.fr>
2022-09-14 11:49:29 +0200

    Récupération des données depuis l'API

--- front/script.js
+++ front/script.js
@@ -1,2 +1,14 @@
 // Envoie de la requête HTTP auprès du service web
 const dataCanape = fetch("http://localhost:3000/api/products");
+// Récupération des données depuis l'API
+dataCanape.then((response) => {
+  const listCanape = response.json();
+  listCanape.then((list) => {
+    for (let i = 0; i < list.length; i++) {
+      console.log(list[i]);
+      const imgCanape = list[i].imageUrl;
+      const nameCanape = list[i].name;
+      const desCanape = list[i].description;
+    }
+  });
+});
